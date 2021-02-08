6acf55adfbd7cc76173623f8ac9de59ba10bddeb
Midka <me@midka.dev>
2021-02-08 18:26:35 +0200

    Refactored cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -51,44 +51,48 @@ async function missingOptionsPrompt(options: Args) {
   console.log(answers.targetDirectory);
 
   // Getting project's name
-  Object.assign(answers, await prompter.getName());
+  addToAnswers(await prompter.getName());
 
   // If not template supplied then asking for it
   if (!options.template) {
-    Object.assign(answers, await prompter.getTemplate());
+    addToAnswers(await prompter.getTemplate());
   }
 
   // If not git argument passed then asking for it
   if (!options.git) {
-    Object.assign(answers, await prompter.getGit());
+    addToAnswers(await prompter.getGit());
   }
 
   // If not install argument passed then asking for it
   if (!options.runInstall) {
-    Object.assign(answers, await prompter.getInstall());
+    addToAnswers(await prompter.getInstall());
 
     // Gettings preferred package manager to install packages
-    Object.assign(answers, await prompter.packageManager());
+    addToAnswers(await prompter.packageManager());
   }
 
   // Getting language for discord bot
   if (answers.template !== 'typescript') {
     if (answers.template !== 'javascript') {
-      Object.assign(answers, await prompter.language());
+      addToAnswers(await prompter.language());
     }
   }
 
   // Asking should i setup typescript
   if (answers.language === 'typescript' || answers.template === 'typescript') {
-    Object.assign(answers, await prompter.setupTs());
+    addToAnswers(await prompter.setupTs());
   }
 
   // Getting bot token and prefix and ownerId
   if (answers.template === 'discordjs') {
-    Object.assign(answers, await prompter.getCredentials());
+    addToAnswers(await prompter.getCredentials());
 
     // Getting ownerId
-    Object.assign(answers, await prompter.getOwnerId());
+    addToAnswers(await prompter.getOwnerId());
+  }
+
+  function addToAnswers(answer: any) {
+    Object.assign(answers, answer);
   }
 
   return answers;
