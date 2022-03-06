b13a071f226bb7512aa01f2c1bc7d6634862f3ad
Midka <me@midka.dev>
2022-03-06 20:22:25 +0200

    example with rust and dialoguer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use dialoguer::{console::Term, theme::ColorfulTheme, Select};
+
 fn main() {
-    println!("Hello, world!");
+    let items = vec!["foo", "bar", "baz"];
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select an item")
+        .items(&items)
+        .default(0)
+        .interact_on_opt(&Term::stderr());
+
+    match selection {
+        Ok(item) => handle_select(item, items),
+        Err(error) => println!("Error: {}", error),
+    }
+}
+
+fn handle_select(selection: Option<usize>, items: Vec<&str>) {
+    match selection {
+        Some(index) => println!("You selected: {:?}", items[index]),
+        None => println!("You canceled the selection"),
+    }
 }
